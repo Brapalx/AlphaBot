2993b32b247c8a62a0667ea7fe0d8b5489f39e74
brapalx <bradymc1998@gmail.com>
2021-02-21 23:03:34 -0800

    asdsa

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ bot.on('messageReactionAdd', (reaction, user) => {
 
             message.channel.send(name + "  WINS!");
 
-            message.channel.overwritePermissions(message.guild.defaultRole, { SEND_MESSAGES: true });
+            message.channel.overwritePermissions(msg.guild.defaultRole, { SEND_MESSAGES: true });
 
 
 
