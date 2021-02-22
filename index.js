3dedc7fdd0c52eed2df6713c7a12db39745e7faa
brapalx <bradymc1998@gmail.com>
2021-02-21 22:37:11 -0800

    asddsa

--- index.js
+++ index.js
@@ -508,7 +508,6 @@ bot.on('message', msg => {
             
             msg.channel.send(pokeEmbed).then( sent => {
                 sent.react('❤️')
-                sent.react('🏆')
             });
 
             var fileString2 = poke_files[Math.floor(Math.random() * poke_files.length)]
@@ -532,7 +531,6 @@ bot.on('message', msg => {
             
             msg.channel.send(pokeEmbed2).then( sent2 => {
                 sent2.react('❤️')
-                sent2.react('🏆')
             });
 
             break;
