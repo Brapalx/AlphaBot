7153875dbb0ed88181402a09c749f419d7ac2d2a
Brady M <bradymc1998@gmail.com>
2022-02-14 17:18:36 -0800

    asss

--- index.js
+++ index.js
@@ -376,7 +376,7 @@ bot.on('message', msg => {
             }
 
             
-            var count = [...guess].reduce((a, e) => { a[e] = a[e] ? a[e] + 1 : 1; return a }, {}); 
+            var count = [...currentWord].reduce((a, e) => { a[e] = a[e] ? a[e] + 1 : 1; return a }, {}); 
             console.log(count);
 
             var outString = "";
