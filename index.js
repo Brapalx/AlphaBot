68a1c8c16b44c328cb2da2a31e9b7cf7d8407c6b
brapalx <bradymc1998@gmail.com>
2022-08-24 22:04:10 -0700

    33

--- index.js
+++ index.js
@@ -301,7 +301,7 @@ bot.on('message', msg => {
             break;
 
         case 'forest':
-            T.get('users/show', { screen_name: `GatorsDaily`}, function (err, data, response) {
+            T.get('users/show', { screen_name: `brapalx`}, function (err, data, response) {
                 if (err) {
                     console.log(`User Fetch Error`);
                     console.log(err);
