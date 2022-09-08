222f8016551efc52da26e95ea07edf5f75eb7b2f
brapalx <bradymc1998@gmail.com>
2022-09-07 20:57:09 -0700

    adssad

---
+++ commands/clear.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('clear')
+		.setDescription('Deletes X number of messages')
+        .addIntegerOption(option =>
+            option.setName('number')
+            .setDescription('Number of messages to delete')
+            .setRequired(true)),
+	async execute(interaction) {
+		await interaction.reply('Pong!');
+        interaction.channel.bulkDelete(option + 1);
+	},
+};
