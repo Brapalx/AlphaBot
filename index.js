84a75844526f383212450b1c2a5584a3ebd079e0
brapalx <bradymc1998@gmail.com>
2022-09-07 19:28:05 -0700

    first stage of transition to v14

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const Discord = require('discord.js')
-const bot = new Discord.Client();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const bot = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 const cheerio = require('cheerio');
 const request = require('request');
