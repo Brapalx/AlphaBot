aa9a54563925b3db383eaeac63f7a8db9269cff5
Brady Menendez <bradymc1998@gmail.com>
2024-03-23 05:15:16 -0700

    asdsda

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ bot.commands = new Collection();
 const mysql = require('mysql2/promise')
 
 const connection = mysql.createConnection({
+  host     : '127.0.0.1',
   user     : process.env.DB_USER,
   password : process.env.DB_PASS,
   database : process.env.DB_NAME
