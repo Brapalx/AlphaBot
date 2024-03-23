168f6f98aa2b3db56d30af186ca001f1f3044372
Brady Menendez <bradymc1998@gmail.com>
2024-03-23 10:50:54 -0700

    adssa

--- commands/poketop.js
+++ commands/poketop.js
@@ -14,42 +14,27 @@ module.exports = {
         const conn = await Index.connection;
 
         const [rows, fields] = await conn.query(
-          `SELECT * FROM Pokemon ORDER BY WINS DESC LIMIT 10`).catch(err => console.log(err));
+          `SELECT * FROM Pokemon ORDER BY WINS DESC LIMIT 20`).catch(err => console.log(err));
 
 
-        console.log(rows[0].STRING);
-        console.log(rows[1].STRING);
+              var concString = "";
+              var tString = "";
+              var i = 1;
 
+              var j;
 
-        // Index.pokeArray.sort((a,b) => parseInt(b.wins) - parseInt(a.wins)); 
+              for (j = 0; j < 20; j++)
+              {
+                  tString = "#" + i.toString() + ":  " + rows[j].STRING + "  -  " + rows[j].WINS + "\n";
+                  concString = concString.concat(tString);
+                  i = i + 1;
+              }
 
-        //       var concString = "";
-        //       var tString = "";
-        //       var i = 1;
+              const pokeWLembed = new EmbedBuilder()
+                .setTitle(" 🥵  __***POKEMON BATTLE WINNER RANKINGS***__  🥵 ")
+                .setDescription(concString);
 
-        //       var j;
-
-        //       for (j = 0; j < 20; j++)
-        //       {
-
-        //         if(!Index.pokeArray[j])
-        //         {
-        //             break;
-        //         }
-
-        //         if(Index.pokeArray[j].wins > 0)
-        //         {
-        //             tString = "#" + i.toString() + ":  " + Index.pokeArray[j].name + "  -  " + Index.pokeArray[j].wins + "\n";
-        //             concString = concString.concat(tString);
-        //             i = i + 1;
-        //         }
-        //       }
-
-        //       const pokeWLembed = new EmbedBuilder()
-        //         .setTitle(" 🥵  __***POKEMON BATTLE WINNER RANKINGS***__  🥵 ")
-        //         .setDescription(concString);
-
-        //       interaction.channel.send({ embeds: [pokeWLembed] });
+              interaction.channel.send({ embeds: [pokeWLembed] });
 
 
         await interaction.deleteReply();
