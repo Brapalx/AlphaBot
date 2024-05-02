6ae967763aa9933258151825c4d421e4b76adfc3
Brady Menendez <bradymc1998@gmail.com>
2024-05-01 20:19:11 -0700

    tsting new command

--- commands/russian.js
+++ commands/russian.js
@@ -9,6 +9,29 @@ module.exports = {
 		const Index = require('../index.js')
 
 
+		var htmldata="";
+        var imgString=""
+        request('https://www.gocomics.com/random/heathcliff', function (error, response, body) {
+
+            htmldata=body;
+			
+			console.log(htmldata);
+                    
+            // let imgIndexStart = htmldata.indexOf("<div id='z'");
+            // imgString = htmldata.substring(imgIndexStart + 12, htmldata.size);
+    
+            // let imgIndexEnd = imgString.indexOf("<br/>");
+    
+            // imgString = imgString.substring(0, imgIndexEnd);
+            // //imgString = imgString.replace(/\s+/g, '');
+            // //console.log(imgIndexStart);
+            // //console.log(imgIndexEnd);
+    
+                    
+            // msg.channel.send(imgString);
+    
+        });
+
 
 		await interaction.reply('Pong!');
 	},
