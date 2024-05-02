746f2449f1aa6f683b1c81a58c553a0f8e8785f3
Brady Menendez <bradymc1998@gmail.com>
2024-05-01 19:43:03 -0700

    updated openapi

--- commands/chatgpt.js
+++ commands/chatgpt.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const { SlashCommandBuilder } = require('discord.js');
 
 //const {ChatGPTAPIBrowser} = require('chatgpt')
-const {Configuration, OpenAIApi} = require('openai');
+const {OpenAI} = require('openai');
 const { message } = require('statuses');
 const { EmbedBuilder } = require('discord.js');
 
@@ -16,31 +16,27 @@ module.exports = {
             .setRequired(true)),
 	async execute(interaction) {
 
-		const configuration = new Configuration({
-            apiKey: process.env.OPENAI_KEY
-        })
 
-        const openai = new OpenAIApi(configuration);
+        const openai = new OpenAI({
+          organization: process.env.OPENAI_ORG,
+          project: process.env.OPENAI_PROJ,
+          api_key: process.env.OPENAI_KEY
+        })
 
         await interaction.reply('Thinking... 🤔');
         
         try {
-            const completion = await openai.createCompletion({
-                model:"gpt-3.5-turbo",
-                prompt: interaction.options.getString('prompt'),
-                temperature:0.7,
-                max_tokens:2048,
-                top_p:1,
-                frequency_penalty:0.0,
-                presence_penalty:0.0,
+            const completion = await openai.chat.completions.create({
+                messages: [{roles: "system", content: interaction.options.getString('prompt')}],
+                model:"gpt-3.5-turbo"
             });
-            console.log(completion.data.choices[0].text);
+            console.log(completion.choices[0]);
 
             const responseEmbed = new EmbedBuilder()
 	            .setColor(interaction.member.displayColor)
                 .setAuthor({ name: interaction.user.tag, iconURL: interaction.user.displayAvatarURL()})
 	            .setTitle(interaction.options.getString('prompt'))
-	            .setDescription(completion.data.choices[0].text)
+	            .setDescription(completion.choices[0])
 	            .setTimestamp()
 	            .setFooter({ text: 'Powered by GPT-3.5 Turbo'});
 
